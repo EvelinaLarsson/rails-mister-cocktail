6cc9e3d24f8a63a29164a4081cbdf3fa9e5831d8
Evelina Larsson <aevelinalarsson@gmail.com>
2019-09-25 19:49:40 +0100

    Heroku update again

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,6 +2,5 @@ class Cocktail < ApplicationRecord
   has_many :doses
   has_many :ingredients, through: :doses, dependent: :destroy
   validates :name, uniqueness: true, presence: true
-  validates :instruction, presence: true
   mount_uploader :photo, PhotoUploader
 end
